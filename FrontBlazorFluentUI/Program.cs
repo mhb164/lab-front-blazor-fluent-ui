976dfa69c4f98116e11c0950e4d06506bc1c2f37
mhb164 <mhb164@gmail.com>
2025-03-17 23:04:12 +0100

    Use DefaultsExtensions

--- FrontBlazorFluentUI/Program.cs
+++ FrontBlazorFluentUI/Program.cs
@@ -4,28 +4,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // Configuration
 var apiOptions = builder.Configuration.GetSection(ApiOptions.ConfigName)?.Get<ApiOptions>();
-builder.Services.AddSingleton(ApiOptions.ToModel(apiOptions));//ApiClientConfig
-
-// Services
-builder.Services.AddSingleton(new JsonSerializerOptions
-{
-    PropertyNameCaseInsensitive = false,
-    PropertyNamingPolicy = null,
-    WriteIndented = true,
-    IncludeFields = false,
-});
-
-builder.Services.AddSingleton<TimeService>();
-builder.Services.AddScoped<IStorageProvider, StorageProvider>();
-builder.Services.AddScoped<UserContext>();
-builder.Services.AddScoped<IAuthStateProvider, AuthStateProvider>();
-builder.Services.AddScoped<AuthenticationStateProvider>(provider => (provider.GetRequiredService<IAuthStateProvider>() as AuthStateProvider)!);
-builder.Services.AddAuthorizationCore();
-
-builder.Services.AddScoped(sp => new HttpClient());
-builder.Services.AddScoped<IApiClient, ApiClient>();
-builder.Services.AddScoped<AuthClient>();
-builder.Services.AddScoped<LayoutService>();
+var apiConfig = ApiOptions.ToModel(apiOptions);
+builder.Services.PrepareDefaults(apiConfig);
 
 builder.Services.AddFluentUIComponents();
 
